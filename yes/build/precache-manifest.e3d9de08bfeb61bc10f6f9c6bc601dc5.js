1764264f065afe09a0ba799e588330c5a0cae76e
kimirooni <59374067+kimirooni@users.noreply.github.com>
2020-10-21 18:25:30 -0400

    Add files via upload

---
+++ yes/build/precache-manifest.e3d9de08bfeb61bc10f6f9c6bc601dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cdfecd6c4c498e38ef26d7873df732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d690a23d5f17aa188554",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e72b8effbee3ad577927",
+    "url": "/static/css/main.56bd4ed1.chunk.css"
+  },
+  {
+    "revision": "d690a23d5f17aa188554",
+    "url": "/static/js/2.81b7e5d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.81b7e5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72b8effbee3ad577927",
+    "url": "/static/js/main.ec6aa934.chunk.js"
+  },
+  {
+    "revision": "7e3f15c85c81d4a8d8fa",
+    "url": "/static/js/runtime-main.7f463016.js"
+  }
+]);
